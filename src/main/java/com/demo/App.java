2c95ff61a7592994d3a0c23e4c2bfc8ff2114d3c
Abhisheik912 <abhisheik912@gmail.com>
2026-06-27 22:25:49 +0530

    Webhook test

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,7 +2,7 @@ package com.demo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI/CD Pipeline!");
+        System.out.println("Hello from Jenkins CI/CD Pipeline - v2!");
     }
 }
 // Trigger Jenkins webhook
